cea4625a98d4c865ad72241ee1585b8a0e7a2cb2
Hashir <hashir.hussain111@gmail.com>
2020-03-27 19:22:02 +0530

    Updates

---
+++ precache-manifest.f518c5e27405da69cf0c9b802304f852.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2bb942359261f5750e8e00b82ade15",
+    "url": "/covid19-geomap/index.html"
+  },
+  {
+    "revision": "78106e23f15c9dda5c6c",
+    "url": "/covid19-geomap/static/css/main.2cd6e4ec.chunk.css"
+  },
+  {
+    "revision": "8ccffe8bb38b217db4d0",
+    "url": "/covid19-geomap/static/js/2.8f6c962d.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/covid19-geomap/static/js/2.8f6c962d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78106e23f15c9dda5c6c",
+    "url": "/covid19-geomap/static/js/main.7aed2d02.chunk.js"
+  },
+  {
+    "revision": "7b766c16319e46a0b542",
+    "url": "/covid19-geomap/static/js/runtime-main.f58b574d.js"
+  }
+]);
